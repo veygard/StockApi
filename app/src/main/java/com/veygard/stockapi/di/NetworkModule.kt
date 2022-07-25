1c4fc2ce5a59760c368d52895d5669547d1db409
Boris_Sadakov <boris.sadakov@sfera.zone>
2022-07-25 10:50:57 +0300

    Added NetworkModule with provide retrofit and stockApi class

---
+++ app/src/main/java/com/veygard/stockapi/di/NetworkModule.kt
@@ -0,0 +1,51 @@
+package com.veygard.stockapi.di
+
+import com.veygard.stockapi.data.api.StockApi
+import com.veygard.stockapi.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideStockApiService(retrofit: Retrofit): StockApi = retrofit.create(StockApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideRemoteClient(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(provideHttpClient())
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .build()
+    }
+
+}
