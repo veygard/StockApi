7d06859a521e15954bb81d7e68548a104fa0013a
Boris_Sadakov <boris.sadakov@sfera.zone>
2022-07-25 16:30:59 +0300

    Added viewModel and viewModelState with fun getStocks()

---
+++ app/src/main/java/com/veygard/stockapi/presentation/viewmodel/StockStateVM.kt
@@ -0,0 +1,9 @@
+package com.veygard.stockapi.presentation.viewmodel
+
+import com.veygard.stockapi.domain.model.StockItem
+
+sealed class StockStateVM {
+    data class GotData(val stocks: List<StockItem>) : StockStateVM()
+    object Loading : StockStateVM()
+    object Error : StockStateVM()
+}
---
+++ app/src/main/java/com/veygard/stockapi/presentation/viewmodel/StockViewModel.kt
@@ -0,0 +1,38 @@
+package com.veygard.stockapi.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.veygard.stockapi.domain.model.StockItem
+import com.veygard.stockapi.domain.response.StockRepositoryResponse.Error
+import com.veygard.stockapi.domain.response.StockRepositoryResponse.Success
+import com.veygard.stockapi.domain.usecase.GetStocksUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class StockViewModel @Inject constructor(val getStocksUseCase: GetStocksUseCase) : ViewModel() {
+
+    private var originalList: List<StockItem>? = null
+    private val _state = MutableLiveData<StockStateVM?>(null)
+    val state: LiveData<StockStateVM?> = _state
+
+    init {
+        getStocks()
+    }
+
+    fun getStocks() {
+        viewModelScope.launch {
+            _state.value = StockStateVM.Loading
+            when (val result = getStocksUseCase.execute()) {
+                is Success -> {
+                    originalList = result.stocks
+                    _state.value = StockStateVM.GotData(result.stocks)
+                }
+                Error -> _state.value = StockStateVM.Error
+            }
+        }
+    }
+}
